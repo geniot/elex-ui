b2a4e0924b33fbcbcf96e8249f312a8b139f4731
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2023-03-19 20:34:03 +0100

    Use no context in test

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_API_URL: '/elex',
+  BASE_API_URL: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_API_URL: 'http://localhost:8000/elex',
+  BASE_API_URL: 'http://localhost:8000',
 };
 
 /*
